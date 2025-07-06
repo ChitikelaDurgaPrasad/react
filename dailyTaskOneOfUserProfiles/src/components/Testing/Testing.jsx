b284c6ac2483c84e2fda1f6cdcede2a49477859e
ChitikelaDurgaPrasad <durgaprasadchitikela123@gmail.com>
2025-07-06 11:49:20 +0530

    new project inital commit name of shoppingcart

--- dailyTaskOneOfUserProfiles/src/components/Testing/Testing.jsx
+++ dailyTaskOneOfUserProfiles/src/components/Testing/Testing.jsx
@@ -1,19 +1,23 @@
 import React, { useCallback, useState } from 'react'
 
 export const Testing = () => {
-    const[num,setNum]= useState(0);
-    const dp = useCallback(()=>{
-        if(num === 0){
-            console.log(`Zero ${num}`);
-        }
-        else{
-            console.log(`Not Zero ${num}`);
-        }
-    },[])
+    const [num, setNum] = useState(0);
+    const promiseOne = new Promise((resolve,reject)=>{
+        setTimeout(()=>{reject("This is first Promise After resolved of One min")},1000)
+    });
+    const promiseTwo = new Promise((resolve,reject)=>{
+        setTimeout(()=>{reject("This is second Promise After resolved of Two min")},2000)
+    });
+    const promiseThree = "This is Third Promise";
+    const promiseFour = new Promise((resolve, reject)=>{
+        reject("This is Fourth Promise");
+    });
+    const result = Promise.all([promiseOne, promiseTwo,promiseThree, promiseFour]).then(res => console.log(res)).catch(error => console.error("Promise.all => ", error));
+
   return (
     <div>
         <div className='d-flex align-items-center justify-content-center'>{num}</div>
-        <button type="button" onClick={()=>{console.log("hello");dp();setNum(prev=>prev+1)}}>Btn</button>
+        <button type="button" onClick={()=>{setNum(prev=>prev+1)}}>Btn</button>
     </div>
   )
 }
